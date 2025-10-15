a674d665590414340434bbeb110ce1efaf920d24
samueljuma <samueljuma1024@gmail.com>
2025-10-15 11:33:25 +0300

    update agp

---
+++ app/src/main/java/com/samueljuma/basenetworkapp/BaseNetworkApplication.kt
@@ -0,0 +1,9 @@
+package com.samueljuma.basenetworkapp
+
+import android.app.Application
+
+class BaseNetworkApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
